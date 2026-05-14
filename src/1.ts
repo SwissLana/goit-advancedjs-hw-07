66c6e6ac7dd90bf11452eb34bdcf6409726f1502
Lana_Huerzeler <huerzeler.lana@gmail.com>
2026-05-14 17:39:00 +0200

    hw7

--- src/1.ts
+++ src/1.ts
@@ -4,16 +4,29 @@
   напишіть скорочену ініціалізацію.
 */
 
-class Student {
-  public name: string;
-  public age: number;
-  public grade: string;
+// class Student {
+//  public name: string;
+//  public age: number;
+//  public grade: string;
 
-  constructor(name: string, age: number, grade: string) {
-    this.name = name;
-    this.age = age;
-    this.grade = grade;
-  }
+//  constructor(name: string, age: number, grade: string) {
+//    this.name = name;
+//    this.age = age;
+//    this.grade = grade;
+//  }
+//}
+
+// Скорочена ініціалізація
+
+export {};
+
+class Student {
+  constructor(
+    public name: string,
+    public age: number,
+    public grade: string,
+  ) {}
 }
 
 export {};
+
